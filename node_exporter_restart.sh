fe151191cfbda1c9d16466a035f44b4d3bb2da6f
Robert Place <robert.r.place@pm.me>
2024-07-07 14:27:20 -0500

    u

--- node_exporter_restart.sh
+++ node_exporter_restart.sh
@@ -3,5 +3,4 @@
 pidof node_exporter
 
 if [[ $? -ne 0 ]] ; then
-    /mnt/rtrusb/node_exporter/node_exporter --web.listen-address=":9100" >>/mnt/rtrusb/node_exporter/node_exporter.log 2>&1 &
-fi
+    /mnt/rtrusb/node_exporter/node_exporter --web.listen-address=":9100" >>/mnt/rtrusb/node_exporter/node_exporter.log 2>&1 & fi
