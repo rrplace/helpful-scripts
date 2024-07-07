240137dddeeb27883fbbc26a16c27e4c5682b133
rrplace <115316283+rrplace@users.noreply.github.com>
2024-07-07 14:36:04 -0500

    Update node_exporter_restart.sh

--- node_exporter_restart.sh
+++ node_exporter_restart.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-pidof node_exporter
-
-if [[ $? -ne 0 ]] ; then
-    /mnt/rtrusb/node_exporter/node_exporter --web.listen-address=":9100" >>/mnt/rtrusb/node_exporter/node_exporter.log 2>&1 & fi
+./node_exporter
